ec779773eb692972883890c0ed858cb47035ba6c
Rafael Amador Galván <rafael.amador@gmail.com>
2022-12-06 08:51:29 -0600

    Linting

--- 2022/day6.py
+++ 2022/day6.py
@@ -18,5 +18,5 @@ def day6(linea, chars=1):
 
 with open(Path(".").joinpath('day6.txt'), 'r') as arch:
     linea = arch.readline()
-    print(day6(linea,4)) # day6 part 1
-    print(day6(linea,14)) # day6 part 2
+    print(day6(linea, 4))  # day6 part 1
+    print(day6(linea, 14))  # day6 part 2
